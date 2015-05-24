8caf10d0205817d432eb5021a47e2b3bf6dfeb01
john-karanja <arakanj@gmail.com>
2015-05-24 10:16:27 +0300

    added paperclip and relationship bwtween category and products

---
+++ app/models/product.rb
@@ -0,0 +1,6 @@
+class Product < ActiveRecord::Base
+  belongs_to :category
+
+  has_attached_file :image, styles: { :medium => "300x300#" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+end
---
+++ app/views/products/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @product, :id, :title, :category_id, :created_at, :updated_at
