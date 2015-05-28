3569a9cefb60b56820c0cdccfedcf64bcd5e1cc4
john-karanja <arakanj@gmail.com>
2015-05-28 04:32:56 +0300

    changed nanbar and other styling and added category links to nav bar

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150527112424) do
+ActiveRecord::Schema.define(version: 20150527122104) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "title"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20150527112424) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.float    "price"
+    t.string   "outlet"
   end
 
   create_table "companies", force: :cascade do |t|
