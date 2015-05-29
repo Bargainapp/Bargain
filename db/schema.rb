9972ec7c001efd3650d8964bffbde544af3b7e76
john-karanja <arakanj@gmail.com>
2015-05-29 04:12:59 +0300

    added location column to products

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150529010855) do
+ActiveRecord::Schema.define(version: 20150529011222) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "title"
@@ -78,6 +78,7 @@ ActiveRecord::Schema.define(version: 20150529010855) do
     t.float    "price"
     t.string   "outlet"
     t.text     "desription"
+    t.string   "location"
   end
 
   add_index "products", ["category_id"], name: "index_products_on_category_id"
